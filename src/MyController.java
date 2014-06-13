41b81c4d092285811e14cb63dba2df34477b22e6
Kazagha <chrisg.local@gmail.com>
2014-06-13 13:05:32 +1000

    MyController: Add load/setup of Conf settings file.

--- src/MyController.java
+++ src/MyController.java
@@ -1,13 +1,40 @@
+import java.io.File;
+
+import net.arcanesanctuary.Configuration.Conf;
 
 public class MyController {
 
 	private MyView view;
+	private Conf conf;
 	
 	public MyController(MyView view)
 	{
 		this.view = view;
 		setActions();
+		conf = loadConf("hierarchy.conf");		
 	}
 	
-	public void setActions(){}
+	public Conf loadConf(String fileName)
+	{
+		Conf tempConf = new Conf(new File(fileName));
+		
+		tempConf.set(new String[] {"Server", "Instance", "Database", "Username", "Password"});
+		tempConf.prompt();
+		tempConf.set("url",  "jdbc:jtds:sqlserver://" + tempConf.get("Server")+ ";instance="+ tempConf.get("Instance") + ";DatabaseName=" + tempConf.get("Database"));
+		
+		return tempConf;
+	}
+	
+	public void saveConf()
+	{
+		conf.nullValues(new String[] {"Password"});
+		conf.save();		
+	}
+	
+	public void setActions(){}	
+	
+	public void testQuery()
+	{
+		SQLQuery sql = new SQLQuery(conf);
+	}
 }
