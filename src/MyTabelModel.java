31d1f2628e23b4728beb1339bebda9b539be1d72
Kazagha <chrisg.local@gmail.com>
2014-06-14 20:39:07 +1000

    MyTabelModel: Initial Commit

---
+++ src/MyTabelModel.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+	class MyTabelModel extends AbstractTableModel
+	{
+		ArrayList<RoleData> dataArray = new ArrayList<RoleData>();
+		String [] columnName = {"Role Num", "Role Description"};
+	
+		@Override
+		public int getColumnCount() {
+			return columnName.length;
+		}
+
+		@Override
+		public int getRowCount() {
+			return dataArray.size();
+		}
+		
+		public String getColumnName(int col)
+		{
+			return columnName[col];
+		}
+
+		@Override
+		public String getValueAt(int row, int col) {
+			RoleData rowData = dataArray.get(row);
+			if(col == 0)
+			{
+				return String.valueOf(rowData.getRole());
+			} else {
+				return rowData.getDescription();
+			}
+		}
+	}
