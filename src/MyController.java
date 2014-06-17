7ff6391caaa2ae5a82c6614050d712108e35a58e
Kazagha <chrisg.local@gmail.com>
2014-06-17 19:17:59 +1000

    MyController: Outline of actionListener

--- src/MyController.java
+++ src/MyController.java
@@ -1,9 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.swing.JTable;
 import javax.swing.table.TableModel;
+
+import com.sun.xml.internal.ws.wsdl.ActionBasedOperationSignature;
 
 import net.arcanesanctuary.Configuration.Conf;
 
@@ -14,16 +18,29 @@ public class MyController {
 	private MyTableModel tableLHS;
 	private MyTableModel tableRHS;
 	
+	static enum Actions {
+		LOADLHS ("loadLHS"),
+		LOADRHS ("LoadRHS"),
+		SWAPSIDES ("SwapSides");
+		
+		String commandString;
+		
+		Actions(String commandString)
+		{
+			this.commandString = commandString;
+		}
+	}
+	
 	public MyController(MyView view)
 	{
 		this.view = view;
-		view.setLHSTitle("User X");
 		tableLHS = view.getLHSTableModel();
 		tableRHS = view.getRHSTableModel();
 		
 		setActions();
 		conf = loadConf("hierarchy.conf");
 		testUserQuery(tableLHS, "Chris", "Green");
+		testUserQuery(tableRHS, "Payden", "Taylor");
 		saveConf();
 	}
 	
@@ -45,7 +62,22 @@ public class MyController {
 		//conf.save();		
 	}
 	
-	public void setActions(){}	
+	public void setActions(){}
+	
+	public class MyActionListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getActionCommand() == "LoadLHS")
+			{
+				
+				//view.setLHSTitle("");
+			} else if (e.getActionCommand() == "LoadRHS")
+			{
+				//view.setRHSTitle("");
+			}
+		}		
+	}
 	
 	public void testUserQuery(MyTableModel tempModel, String firstName, String lastName)
 	{
