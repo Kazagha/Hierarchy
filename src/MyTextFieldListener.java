64b4d9be5ccaff8ad36856a26bd18dc8237beb2e
Kazagha <chrisg.local@gmail.com>
2014-07-11 12:47:38 +1000

    MyTextFieldListener: Update to locate a match from the array.

--- src/MyTextFieldListener.java
+++ src/MyTextFieldListener.java
@@ -1,10 +1,26 @@
+import java.awt.TextField;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
 
-
 public class MyTextFieldListener implements DocumentListener {
+	
+	private ArrayList<String> userNameArray = new ArrayList<String>();
+	private static enum Mode { INSERT, COMPLETION };
+	private Mode mode = Mode.INSERT;
+	private JTextField textField;
+	
+	//public MyTextFieldListener(ArrayList<String> userNameList) {
+	public MyTextFieldListener() {
+		//Create some dummy names in the array
+		userNameArray.add("anthony");
+		userNameArray.add("chris");
+		userNameArray.add("fred");
+	}
 
 	@Override
 	public void changedUpdate(DocumentEvent ev) {	
@@ -41,25 +57,64 @@ public class MyTextFieldListener implements DocumentListener {
 			e.printStackTrace();
 		}
 		
-		//Set 'w' as the starting character of the word
+		// Set 'w' as the starting character of the word
 		int w;
 		for (w = pos; w >= 0; w--)
 		{
-			//Break on a non-letter character
+			// Break on a non-letter character
 			if (! Character.isLetter(content.charAt(w)))
 			{
 			break;
 			}
 		}
 		
-		//Not enough characters
+		// Not enough characters
 		if ((pos - w) < 2)
 		{
 			return;
 		}
 		
-		System.out.println(w);
+		String prefix = content.substring(w + 1).toLowerCase();
+		int n = Collections.binarySearch(userNameArray, prefix);
+		if(n < 0 && -n <= userNameArray.size())
+		{
+			//System.out.println("Match?");
+			String match = userNameArray.get(-n - 1);
+			if (match.startsWith(prefix))
+			{
+				// Completion is found
+				String completion = match.substring(pos - w);
+				// Cannot modify the Document from within the notification, 
+				// submit a task to do the change later
+			}
+		} else {
+			//Nothing Found
+			mode = Mode.INSERT;
+		}
 		
-		System.out.println(content);
+		//System.out.println("n: " + n + " Array Size: " + userNameArray.size());
+		//System.out.println(prefix);
+	}	
+	
+	private class CompletionTask implements Runnable {
+		String completion;
+		int position;
+		
+		CompletionTask(String completion, int position)
+		{
+			this.completion = completion;
+			this.position = position;
+		}
+		
+		public void run() {
+			textField.setText(textField.getText());
+		}
+	}
+	
+	private String insert(String text, String completion, int position)
+	{
+		StringBuilder sb = new StringBuilder(text);
+		sb.insert(position, completion);
+		return sb.toString();
 	}
 }
