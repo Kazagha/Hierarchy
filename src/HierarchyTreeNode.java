31faaed4da81068e0ea5559b3c653fbd372e8d82
Kazagha <chrisg.local@gmail.com>
2014-09-06 10:02:49 +1000

    HierarchyTreeNode: Swapped one mode for two. - The new enum modes are 'activeMode' and 'selectedMode'. - Setup new setter/getter methods.

--- src/HierarchyTreeNode.java
+++ src/HierarchyTreeNode.java
@@ -2,34 +2,44 @@ import javax.swing.tree.DefaultMutableTreeNode;
 
 public class HierarchyTreeNode extends DefaultMutableTreeNode {
 
-	public static enum Mode { INACTIVE, PARTIAL_ACTIVE, PARTIAL_SELECTED, ACTIVE, SELECTED };
+	public static enum ActiveMode { INACTIVE, PARTIAL_ACTIVE, ACTIVE };
+	public static enum SelectedMode { NOT_SELECTED, PARTIAL_SELECTED, SELECTED };
 	private static final int INACTIVE = 0;
 	private static final int PARTIAL_ACTIVE = 1;
-	private static final int PARTIAL_SELECTED = 2;
-	private static final int ACTIVE = 3;
-	private static final int SELECTED = 4;	
+	private static final int ACTIVE = 2;
+	private static final int NOT_SELECTED = 0;
+	private static final int PARTIAL_SELECTED = 1;
+	private static final int SELECTED = 2;	
 	
-	Mode mode;
+	ActiveMode activeMode;
+	SelectedMode selectedMode;
 	HierarchyData value;
 	
 	public HierarchyTreeNode(HierarchyData hd)
 	{
-		mode = Mode.INACTIVE;
+		activeMode = ActiveMode.INACTIVE;
+		selectedMode = SelectedMode.NOT_SELECTED;
 		this.userObject = hd;
 	}
 	
-	public void setMode(Mode newMode)
+	public void setMode(Object newMode)
 	{
-			this.mode = newMode;
+		if(newMode instanceof ActiveMode)
+		{			
+			this.activeMode = (HierarchyTreeNode.ActiveMode) newMode;
+		} else if(newMode instanceof SelectedMode) 
+		{
+			this.selectedMode = (HierarchyTreeNode.SelectedMode) newMode;
+		}
 	}
 	
-	public void setInactive()
+	public ActiveMode getActiveMode()
 	{
-		this.mode = Mode.INACTIVE;
+		return this.activeMode;
 	}
 	
-	public Mode getMode()
+	public SelectedMode getSelectedMode()
 	{
-		return this.mode;
+		return this.selectedMode;
 	}
 }
