4ca903725cf1c0b14aee77104260541a6de6c25f
Kazagha <chrisg.local@gmail.com>
2014-10-26 16:07:21 +1000

    MySearchDialog: Clean and rearrange code, set up 'showDialog' method.

--- src/MySearchDialog.java
+++ src/MySearchDialog.java
@@ -7,25 +7,37 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
 public class MySearchDialog extends JPanel {
 
+	//Frame Width and Height
+	final int FRAME_WIDTH = 150;
+	final int FRAME_HEIGHT = 150;
+	
+	JFrame searchFrame = new JFrame();
+	
+	JButton nextButton = new JButton("Next");
+	JButton prevButton = new JButton("Previous");
+	JTextField searchTextField;
 	//public MySearchDialog(String title, Object[] content, JButton[] options)
-	public MySearchDialog(String titleString)
+	public MySearchDialog(String titleString, JTextField searchTextField)
 	{
-		//Frame Width and Height
-		final int FRAME_WIDTH = 150;
-		final int FRAME_HEIGHT = 150;
+		this.searchTextField = searchTextField;
 		
-		JFrame searchFrame = new JFrame(titleString);		
+		searchFrame.setTitle(titleString);
 		searchFrame.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
 		
 		JPanel contentPanel = new JPanel();
-		contentPanel.add(new JLabel("THis is a test"));
+		contentPanel.add(new JLabel("Enter Search String"));
+		contentPanel.add(searchTextField);
 		
 		searchFrame.setContentPane(contentPanel);
 		searchFrame.pack();			
-		searchFrame.setVisible(true);	
+		//searchFrame.setVisible(true);	
+	}
+	
+	public void showDialog()
+	{
+		searchFrame.setVisible(true);
 	}
 	
 }
