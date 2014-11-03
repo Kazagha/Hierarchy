9763f91ce093904fc5a7728ac847b0d863e04baf
Kazagha <chrisg.local@gmail.com>
2014-11-03 15:32:56 +1000

    CSVFilter: Update comments.

--- src/CSVFilter.java
+++ src/CSVFilter.java
@@ -4,27 +4,31 @@ import javax.swing.filechooser.FileFilter;
 
 public class CSVFilter extends FileFilter {
 
-	public CSVFilter() {
-		// TODO Auto-generated constructor stub
-	}
+	public CSVFilter() {}
 
 	@Override
 	public boolean accept(File f) {
+		// Show all directories
 		if (f.isDirectory())
 		{
 			return true;
 		}
 		
+		// Get the file name
 		String nameString = f.getName();
+		// Find the position of the last '.' character
 		int lastIndexOf = nameString.lastIndexOf(".");
+		
 		String extensionString = null;
 		
 		// Check that a file extension exists
 		if(lastIndexOf > 0)
 		{
+			// Find the extension
 			extensionString = nameString.substring(lastIndexOf);
 		}
 				
+		// If the extension exists, is it a '.CSV' extension
 		if (extensionString != null)
 			if (extensionString.equalsIgnoreCase(".csv"))
 			{
